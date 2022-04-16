7141af60483f5e7a7258514722fea88222f294fa
JOHNCKHSIAO <johnckhsiao@gmail.com>
2022-04-17 01:00:43 +0800

    0417

--- JS/myproject.js
+++ JS/myproject.js
@@ -1,35 +1,11 @@
-// function logoTop(){
-// document.querySelector("#footer_logoid").addEventListener("click",function(){
-//     Window.scrollTo({
-//         top: 0,
-//         behavior: "smooth"
-//     });
-// })
-// }
-// logoTop();
-//這是測試用的
-(function () {
-
-    let btn=document.querySelector("#float");
-
-    btn.addEventListener("click",function(){
-        topFunction();
-        // alert("hi");
+(function(){
+    let footerLogo=document.querySelector("#footer_logoid");
+    footerLogo.addEventListener("click",function(){
+        // alert("test");
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+          });
     })
-
-    window.onscroll = function(){
-        scrollFunction();
-    };
-
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        btn.style.display = "block";
-        } else {
-            btn.style.display = "none";
-        }
-    }
-
-    function topFunction() {
-        document.documentElement.scrollTop = 0; 
-    }
 })();
