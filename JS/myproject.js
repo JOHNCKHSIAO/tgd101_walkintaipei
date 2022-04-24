0f1cbe846790658374b6bc58e140db6bd89f4b3c
JOHNCKHSIAO <johnckhsiao@gmail.com>
2022-04-25 00:53:09 +0800

    flex wrap rwd

--- JS/myproject.js
+++ JS/myproject.js
@@ -9,3 +9,59 @@
           });
     })
 })();
+
+
+
+// let i = 1;
+// let slide=document.querySelector("#slideimg");
+
+// setInterval(function(){
+
+//     if(i<5){
+        
+//         i++;
+//     }else{
+//         i=1;
+//     }
+
+//     slide.src=`./image/index/slide${i}.jpg`;
+
+// },1000);
+
+function changeImg(){
+    let i=0;
+    let slide=document.querySelector("#slideimg");
+    setInterval(function(){
+        if(i<5){
+            i++;
+        }else{
+            i=1;
+           
+        }
+        slide.src=`./image/index/slide${i}.jpg`;
+    },2000)
+}
+
+
+
+// changeImg();
+
+
+// changeImg();
+
+// function changeImg(){
+ 
+//     let slide=document.querySelector("#slideimg");
+//     //console.log(slide.src);
+//     //for (let i=1;i<6;i++){
+//         let i = 1;
+//         setInterval(function(){
+//             slide.src=`./image/index/slide${i}.jpg`;
+//             i++;
+//             console.log(i);
+//         },3000);
+//     //}
+
+   
+// }   
+        
